fcf5c0f8607016c569e10df38118b0ede20746e7
Caio Henrique da Silva <caio.henrique@unimedcg.coop.br>
2024-06-20 11:36:02 -0300

    change header and main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export function Header({ children = <h1>Conteúdo do Header</h1> }) {
   return (
     <div
       className={cn(
-        "h-12 p-2 bg-gray-100 text-center text-xl font-semibold",
+        "h-12 p-2 bg-green-700 text-center text-xl font-semibold",
         "flex flex-row items-center justify-center"
       )}
     >
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,3 @@
 export function Main({ children = <p>Conteúdo de Main</p> }) {
-  return <main className="p-2">{children}</main>
+  return <main className="p-2">{children}</main>;
 }
